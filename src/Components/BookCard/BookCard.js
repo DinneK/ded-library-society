2ab897af69e9e9cb72585a733033bdcb2fc068be
Emily Miles <ms.emily.j.miles@gmail.com>
2022-11-02 15:24:46 -0400

    Remove commented out code

--- src/Components/BookCard/BookCard.js
+++ src/Components/BookCard/BookCard.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './BookCard.css';
 
 const BookCard = ({ id, cover, title }) => {
-  // console.log(id)
   return (
-    // <div className="book-card" id={ id } key={ id }>
-      <Link to={`/books${id}`} className="book-card" id={ id } key={ id }>
-        <img src={ cover } alt={ `${title} Book Cover` } />
-        <h2>{ title }</h2>
-        <button>❤️</button>
-      </Link>
-      // </div>
+    <Link to={ `/books${id}` } className="book-card" id={ id } key={ id }>
+      <img src={ cover } alt={ `${title} Book Cover` } />
+      <h2>{ title }</h2>
+      <button>❤️</button>
+    </Link>
   );
 };
 
