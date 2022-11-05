2b1bf8e97623af1836f607dd3be0567bd7f6e530
Emily Miles <ms.emily.j.miles@gmail.com>
2022-11-05 14:14:06 -0400

    Refactor conditional rendering for dryness

--- src/Components/BookCard/BookCard.js
+++ src/Components/BookCard/BookCard.js
@@ -1,36 +1,29 @@
+import "./BookCard.css";
+
 import React from "react";
 import { Link } from "react-router-dom";
-import "./BookCard.css";
+
 import { useSelector, useDispatch } from "react-redux";
 import { saveBook, deleteBook } from "../../features/saveBook/saveBookSlice";
-import saveBookSlice from "../../features/saveBook/saveBookSlice";
+// import saveBookSlice from "../../features/saveBook/saveBookSlice";
 
 const BookCard = ({ id, cover, title }) => {
   const dispatch = useDispatch();
   const booksArr = useSelector((state) => state.savedBooks);
 
-  if (!booksArr.savedBooks.includes(id)) {
-    return (
-      <div>
-        <Link to={`/books${id}`} className="book-card" id={id} key={id}>
-          <img src={cover} alt={`${title} Book Cover`} />
-          <h2>{title}</h2>
-        </Link>
-        <button onClick={() => dispatch(saveBook(id))}>❤️</button>
-      </div>
-    );
-  }
-  if (booksArr.savedBooks.includes(id)) {
-    return (
-      <div>
-        <Link to={`/books${id}`} className="book-card" id={id} key={id}>
-          <img src={cover} alt={`${title} Book Cover`} />
-          <h2>{title}</h2>
-        </Link>
-        <button onClick={() => dispatch(deleteBook(id))}>🗑</button>
-      </div>
-    );
-  }
+
+  return (
+    <div>
+      <Link to={ `/books${id}` } className="book-card" id={ id } key={ id }>
+        <img src={ cover } alt={ `${title} Book Cover` } />
+        <h2>{ title }</h2>
+      </Link>
+      { !booksArr.savedBooks.includes(id)
+        && <button onClick={ () => dispatch(saveBook(id)) }>❤️</button> }
+      { booksArr.savedBooks.includes(id)
+        && <button onClick={ () => dispatch(deleteBook(id)) }>🗑</button> }
+    </div>
+  );
 };
 
 export default BookCard;
